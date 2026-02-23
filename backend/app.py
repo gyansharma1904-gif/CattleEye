34459e5f9ddfecb6af3816c0ff3ffd1fce92565e
gyansharma1904-gif <gyansharma1904@gmail.com>
2026-02-23 20:21:46 +0530

    Setup Flask backend with predict API endpoint

---
+++ backend/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    if 'image' not in request.files:
+        return jsonify({"error": "No image uploaded"}), 400
+    
+    file = request.files['image']
+    
+    return jsonify({
+        "message": "API working",
+        "filename": file.filename
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
