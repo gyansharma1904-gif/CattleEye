6a17ed1278b79c62182f669fe3143abec4fb73f2
gyansharma1904-gif <gyansharma1904@gmail.com>
2026-02-23 21:44:18 +0530

    Setup Flask backend with predict API endpoint

--- backend/app.py
+++ backend/app.py
@@ -13,6 +13,6 @@ def predict():
         "message": "API working",
         "filename": file.filename
     })
-
+    
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
